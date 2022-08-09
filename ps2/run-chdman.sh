e23ef40875fe1f8bc8a3dca6531f8a067b6ab3c4
Junian Triajianto <author@junian.net>
2022-08-09 09:01:43 +0700

    Update with cue to chd

--- ps2/run-chdman.sh
+++ ps2/run-chdman.sh
@@ -5,3 +5,12 @@ find . -name "*.iso" -exec chdman createcd -i {} -o {}.chd \; -exec rm {} \;
 
 # Rename .iso.chd to .chd
 for f in *.iso.chd; do mv -- "$f" "${f%.iso.chd}.chd"; done
+
+# Convert .cue to .chd then remove .cue
+find . -name "*.cue" -exec chdman createcd -i {} -o {}.chd \; -exec rm {} \;
+
+# Rename .cue.chd to .chd
+for f in *.cue.chd; do mv -- "$f" "${f%.cue.chd}.chd"; done
+
+# Remove .bin
+find . -name "*.bin" -exec rm {} \;
